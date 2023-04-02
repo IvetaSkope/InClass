9966864fa0c1121b3d0b7c03d74fc82a6af0ad96
Iveta  Skope <iveta.skope@gmail.com>
2023-04-03 00:09:52 +0300

    28.03. changes

--- Lesson4(28.03.).playground/Contents.swift
+++ Lesson4(28.03.).playground/Contents.swift
@@ -1,27 +1,27 @@
-import Cocoa
+import UIKit
 
-//MARK: - OOP Object Orienting Programming
-
+//MARK: - OOP
 
 //blog
-//let postTitle = "OOP"\
-//let posText = "OOP OOP OOP"
+//let postTitle = "OOP"
+//let postText = "OOP OOP OOP"
 //let postAuthor = "AM"
-
-//let postTitle2 = "Classes OOP"
+//
+//let postTitle2 = "Classes"
 //let postText2 = "Classes OOP"
-//let postAuthor2 postAuthor
+//let postAuthor2 = postAuthor
 
-class Post
-    var postTitle = ""
+class Post {
+    var postTitle = "Post Title"
     var postText = ""
     var postAuthor = ""
+//    var email: String?
 }
 
 let firstPost = Post()
 
 firstPost.postTitle = "This is my first post"
-firstPost.postText = "Texthere"
+firstPost.postText = "Text here"
 firstPost.postAuthor = "AA"
 
 let secondPost = firstPost
@@ -31,15 +31,18 @@ secondPost.postText = "New post"
 
 firstPost === secondPost
 
+
 class Comment {
-    var like = ""
-    var author =  ""
+    var like = " "
+    var author = " "
+    
     var numberOfComments = 0
     
     func addComment(){
-        numberOfComments +=1
+        numberOfComments += 1
     }
 }
+
 
 let firstComment = Comment()
 
@@ -47,51 +50,97 @@ firstComment.addComment()
 firstComment.addComment()
 firstComment.addComment()
 
+
 class Human {
     
-    var name: String
-    var age: Int
+    var myName: String
+    var myAge: Int
     
-    init(name:String, age: Int){
+    
+    init(name: String, age: Int){
         self.myName = name
         self.myAge = age
-        }
     }
+}
+
 
 let person = Human(name: "Tod", age: 22)
 person.myAge
 
-//let newPerson = Human() {
+//let newPerson = Human()
 
-class Parent
-<#T##Int#>
-var: myName: String
-var myAge: Int
-
-init(name:String, age: Int){
-    self.myName = name
-    self.myAge = age
+class Parent {
     
+    var myName: String
+    var myAge: Int
+    
+    
+    init(name: String, age: Int){
+        self.myName = name
+        self.myAge = age
+    }
+    
+    func walk(){
+        print("can walk")
+    }
+   
 }
-func walk(){
-    print("can walk")
-}
-}
+
 
 let dad = Parent(name: "Sam", age: 40)
 
 dad.myName
-dad.Walk()
+dad.walk()
 
 
-Class Child: Parent {
+class Child: Parent {
     
     func feeding(){
-        if myAge <= 2[
+        if myAge <= 2{
             print("need a help")
-            }else{
-            print:("can eat independenty")
-            
-                ]
-     
-      func reduce(into: <#T##Result#>, <#T##(Result, Self.Element) -> Void#>)
+        }else{
+            print("can eat independently")
+        }
+    }
+    
+    func educating(){
+        if myAge <= 4{
+            print("kinder age")
+        }else{
+            print("ready to go to school")
+        }
+    }
+    
+}
+
+let littleBoy = Child(name: "Tom", age: 2)
+littleBoy.walk()
+
+
+
+
+class Figure {
+    func draw() {}
+}
+
+
+
+class Circle: Figure {
+    
+    override func draw() {
+        print("Draw the circle")
+    }
+    
+}
+
+class Rectangle: Figure {
+    
+    override func draw() {
+        print("Draw the Rectangle")
+    }
+    
+}
+
+
+let rectangle = Rectangle()
+rectangle.draw()
